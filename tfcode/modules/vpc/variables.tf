0eb25c9582281317c87e96a5db9e970f11412229
Imran Sayyed <imran.sayyed@blazeclan.com>
2023-08-17 19:21:06 +0530

    code update

--- tfcode/modules/vpc/variables.tf
+++ tfcode/modules/vpc/variables.tf
@@ -2,79 +2,77 @@
 variable "environment_name" {
   type        = string
   description = "Set environment name"
-  default     = "Staging"
+  default     = ""
 }
 
 variable "vpc_cidr" {
   type        = string
   description = "VPC CIDR"
-  default     = "10.1.0.0/16"
+  default     = ""
 }
 
 variable "availability_zone" {
   type        = list(string)
   description = "Availability Zone"
-  default     = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+  default     = []
 }
 
 variable "additional_tags" {
   type        = map(string)
   description = "Variable if additional tags is needed"
-  default = {
-    Owner            = "TJ"
-    TerraformManaged = "True"
-  }
+  default = {}
 }
 
 variable "public_subnet_cidr_az_a" {
   type        = list(string)
   description = "Public subnet CIDR within AZ-a"
-  default     = ["10.1.0.0/22"]
+  default     = []
 }
 
 variable "public_subnet_cidr_az_b" {
   type        = list(string)
   description = "Public subnet CIDR within AZ-b"
-  default     = ["10.1.4.0/22"]
+  default     = []
 }
 
 variable "public_subnet_cidr_az_c" {
   type        = list(string)
   description = "Public subnet CIDR within AZ-c"
-  default     = ["10.1.8.0/22"]
+  default     = []
 }
 
 variable "public_subnet_interfix" {
   type        = string
   description = "Give interfix to public subnet name"
-  default     = "public"
+  default     = ""
 }
 
 variable "private_subnet_cidr_az_a" {
   type        = list(string)
   description = "Private subnet CIDR within AZ-a"
-  default     = ["10.1.12.0/22"]
+  default     = []
 }
 
 variable "private_subnet_cidr_az_b" {
   type        = list(string)
   description = "Private subnet CIDR within AZ-b"
-  default     = ["10.1.16.0/22"]
+  default     = []
 }
 
 variable "private_subnet_cidr_az_c" {
   type        = list(string)
   description = "Private subnet CIDR within AZ-c"
-  default     = ["10.1.20.0/22"]
+  default     = []
 }
 variable "private_subnet_interfix" {
   type        = string
   description = "Give interfix to private subnet name"
-  default     = "private"
+  default     = ""
 }
 
 variable "default_route" {
   type        = string
   description = "Default Route from and to internet"
-  default     = "0.0.0.0/0"
+  default     = ""
 }
+
